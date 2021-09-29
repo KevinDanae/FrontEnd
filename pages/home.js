49d7637ea4c8f9e6710ba13be28230bee225cc35
agustinrobledo <agustinrobledo59.ar@gmail.com>
2021-09-28 21:37:30 -0300

    Landing

---
+++ pages/home.js
@@ -0,0 +1,27 @@
+import Head from "next/head";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addCart, getProducts } from "../actions";
+import Cards from "../components/Cards";
+import Filters from "../components/Filters";
+import Carousel from "../components/Carousel";
+import LandingPage from "../components/LandingPage";
+import Navbar from "../components/Navbar";
+
+export default function Home() {
+    const dispatch = useDispatch();
+    useEffect(() => {
+      dispatch(getProducts());
+      dispatch(addCart());
+  
+    }, []);
+  
+    return (
+      <main>
+        <Navbar />
+        <Filters />
+        <Carousel />
+         <Cards />
+      </main>
+    );
+  }
