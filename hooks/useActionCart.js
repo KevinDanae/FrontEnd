cb3be0e824c71e62ad8a1d8fc46b159e7caa3b51
KevinDanae <kevindanaea@gmail.com>
2021-10-03 18:31:39 -0500

    Carrito funcional

--- hooks/useActionCart.js
+++ hooks/useActionCart.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import useCart from "./useCart";
 
-const useActionCart = async (idProduct, action) => {
-    const id = localStorage.getItem("idCart");
-    if(id) {
-        const response = await fetch(`https://wines-db.herokuapp.com/cart?id=${id}`, {
+const useActionCart = async (idProduct, action, payment) => {
+  const id = localStorage.getItem("idCart");
+  if (id) {
+    if (idProduct) {
+      const response = await fetch(
+        `https://wines-db.herokuapp.com/cart?id=${id}`,
+        {
           method: "PUT",
           headers: {
             Accept: "application/json",
@@ -13,13 +17,53 @@ const useActionCart = async (idProduct, action) => {
             action,
             idProduct,
           }),
-        });
-        const data = await response.json();
-        console.log(data);
-        return data
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      return data;
+    } else if (action === "cancelled") {
+      const response = await fetch(
+        `https://wines-db.herokuapp.com/cart?id=${id}`,
+        {
+          method: "PUT",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            action,
+          }),
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      localStorage.removeItem("idCart");
+      useCart();
+      return data;
     } else {
-        console.log('No hay carrito aun');
+      const response = await fetch(
+        `https://wines-db.herokuapp.com/cart?id=${id}`,
+        {
+          method: "PUT",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            action,
+            payment_method: 'stripe',
+          }),
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      localStorage.removeItem("idCart");
+      return data;
     }
-}
+  } else {
+    console.log("No hay carrito aun");
+  }
+};
 
-export default useActionCart
+export default useActionCart;
